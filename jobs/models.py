fc2652eea9454f8e98be5a6388d046d469bdb8e0
Wesley Clark <iamwesleyclark@gmail.com>
2019-04-12 19:56:01 -0400

    Jobs app

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
+
 # Create your models here.
