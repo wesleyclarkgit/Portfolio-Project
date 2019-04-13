95ea97c05c8196ab49af92afd800f7bc85cc3c90
Wesley Clark <iamwesleyclark@gmail.com>
2019-04-13 08:55:37 -0400

    bootstrapped

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
 # Create your models here.
